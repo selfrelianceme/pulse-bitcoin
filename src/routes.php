dc483d160b8e08987603229f777f10bae3f55fbb
Vladislav <vladwebkit@gmail.com>
2018-02-09 16:30:42 +0200

    bag fix

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,4 @@
 <?php
 
 Route::post('pulsebitcoin/cancel', 'Selfreliance\PulseBitcoin\PulseBitcoin@cancel_payment')->name('pulsebitcoin.cancel');
-Route::get('pulsebitcoin/confirm', 'Selfreliance\PulseBitcoin\PulseBitcoin@validateIPNRequest')->name('pulsebitcoin.confirm');
+Route::post('pulsebitcoin/confirm', 'Selfreliance\PulseBitcoin\PulseBitcoin@validateIPNRequest')->name('pulsebitcoin.confirm');
